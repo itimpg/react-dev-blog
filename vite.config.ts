297570944b9e4ae531e71c7c5a12702dba6f85a7
COMPANY\cchairin <cchairin@manaosoftware.com>
2025-07-07 13:50:04 +0700

    add tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/react-dev-blog/',
-  plugins: [react()],
+  plugins: [
+    tailwindcss(),
+    react()
+  ],
   build: {
     outDir: 'dist'
   }
